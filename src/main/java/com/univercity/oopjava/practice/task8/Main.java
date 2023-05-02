f7fe33d5aff36ddfc2b0c5efb2c09e02a3b13256
sergiykatriuk <sergiy.katriuk@gmail.com>
2023-05-02 20:05:33 +0300

    fixed practice 8

--- src/main/java/com/univercity/oopjava/practice/task8/Main.java
+++ src/main/java/com/univercity/oopjava/practice/task8/Main.java
@@ -7,7 +7,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Point> pointArrayList = new ArrayList<>();
-        TreeSet<Point> pointTreeSet = new TreeSet<>();
+        TreeSet<Point> pointTreeSet = new TreeSet<>(new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                return o1.getY() - o2.getY();
+            }
+        });
 
         Point point1 = new Point(1, 2);
         Point point2 = new Point(1, 2);
@@ -21,21 +26,24 @@ public class Main {
         int count = 10;
         Random random = new Random();
         for (int i = 0; i < count; i++) {
-            Point point = new Point(random.nextInt(), random.nextInt());
+            Point point = new Point(random.nextInt(100), random.nextInt(100));
             pointArrayList.add(point);
             pointTreeSet.add(point);
         }
 
         System.out.println("Index of point is " + pointArrayList.indexOf(point1)); // Index of point is 0
         Collections.sort(pointArrayList);
-        System.out.println("After sort index of point is " + pointArrayList.indexOf(point1)); // After sort index of point is 9
+        System.out.println(
+                "After sort index of point is " + pointArrayList.indexOf(point1)); // After sort index of point is 9
         System.out.println("ArrayList:");
         for (Point point : pointArrayList) {
             System.out.println(point);
         }
 
-        System.out.println("TreeSet contains point [1,2] " + pointTreeSet.contains(new Point(1, 2))); // TreeSet contains point [1,2] true
-        System.out.println("TreeSet contains point [3,5] " + pointTreeSet.contains(new Point(3, 5))); // TreeSet contains point [3,5] false
+        System.out.println("TreeSet contains point [1,2] "
+                + pointTreeSet.contains(new Point(1, 2))); // TreeSet contains point [1,2] true
+        System.out.println("TreeSet contains point [3,5] "
+                + pointTreeSet.contains(new Point(3, 5))); // TreeSet contains point [3,5] false
         System.out.println("TreeSet:");
         for (Point point : pointTreeSet) {
             System.out.println(point);
